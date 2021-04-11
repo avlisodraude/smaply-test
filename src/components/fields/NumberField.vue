4ddd4793ba62f46381c7a83495a307b12fc1fe2f
E.Y. Silva Navarrete <>
2021-04-11 22:32:37 +0300

    Add number input field

--- src/components/fields/NumberField.vue
+++ src/components/fields/NumberField.vue
@@ -1,9 +1,27 @@
 <template>
   <div>
     <header>Number <font-awesome-icon icon="cog" /></header>
-    <p>12</p>
+    <p v-show="!inputField" v-on:click="editNumber">Type your number</p>
+    <input v-show="inputField" type="number" v-model="number">
   </div>
 </template>
+<script>
+export default {
+  name: 'NumberField',
+  data() {
+    return {
+      inputField: false,
+      number: null,
+    };
+  },
+  methods: {
+    editNumber() {
+      console.log('edit');
+      this.inputField = !this.inputField;
+    },
+  },
+};
+</script>
 <style scoped lang="scss">
 header {
   color: #999;
@@ -22,5 +40,11 @@ img {
   display:block;
   margin:auto;
   padding: 10px;
+}
+input[type="number"] {
+  border: 1px solid blue;
+  border-radius: 5px;
+  padding: 8px;
+  margin: 0px 10px;
 }
 </style>
